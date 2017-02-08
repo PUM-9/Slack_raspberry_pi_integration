19f26a8f7e634a3bca4e308364f3c44d99c5622c
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-08 13:00:26 +0100

    changed script so that if no internet connection is available it will keep trying 15 times with 1 minute wait in between

--- ip_notifier.py
+++ ip_notifier.py
@@ -1,11 +1,22 @@
 import netifaces as ni
 import requests
 from config import WEBHOOK, INTERFACE
+import time
 
-if __name__ == "__main__":
+
+def get_ip():
     try:
         ip = ni.ifaddresses(INTERFACE)[ni.AF_INET][0]['addr']
     except KeyError:
-        ip = "Saknar wifi"
+        ip = None
+    return ip
+
+if __name__ == "__main__":
+    tries = 0
+    ip = get_ip()
+    while not ip and tries < 15:
+        time.sleep(60)
+        ip = get_ip()
+        tries += 1
     payload = {'text': ip, 'username': 'pi-bot', 'icon_emoji': ':robot_face:'}
     requests.post(WEBHOOK, json=payload)
