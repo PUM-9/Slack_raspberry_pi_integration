8fac5078657a9b63ecb8c66938becc9e5bde9c56
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-08 11:13:51 +0100

    moved out webhook...

--- ip_notifier.py
+++ ip_notifier.py
@@ -1,11 +1,10 @@
 import netifaces as ni
 import requests
-
-WEBHOOK = "https://hooks.slack.com/services/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"
+from config import WEBHOOK, INTERFACE
 
 if __name__ == "__main__":
     try:
-        ip = ni.ifaddresses('wlp2s0')[ni.AF_INET][0]['addr']
+        ip = ni.ifaddresses(INTERFACE)[ni.AF_INET][0]['addr']
     except KeyError:
         ip = "Saknar wifi"
     payload = {'text': ip, 'username': 'pi-bot', 'icon_emoji': ':robot_face:'}
